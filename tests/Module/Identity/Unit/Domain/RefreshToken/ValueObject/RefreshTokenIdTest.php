9bc1e1da5da487e9eeb87e5b993593a2228b184d
silex <a@a.aa>
2025-11-11 08:28:23 +0400

    test

---
+++ tests/Module/Identity/Unit/Domain/RefreshToken/ValueObject/RefreshTokenIdTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\RefreshToken\ValueObject;
+
+use App\Module\Identity\Domain\RefreshToken\ValueObject\RefreshTokenId;
+use Tests\Shared\UnitTestCase;
+
+class RefreshTokenIdTest extends UnitTestCase
+{
+    /**
+     * @return void
+     */
+    public function testItShouldCreateARefreshTokenId(): void
+    {
+        $id = $this->faker->numberBetween(1, PHP_INT_MAX);
+        $refreshTokenId = RefreshTokenId::fromRaw($id);
+        $this->assertEquals($id, $refreshTokenId->toRaw());
+    }
+
+}
---
+++ tests/Module/Identity/Unit/Domain/RefreshToken/ValueObject/TokenTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\RefreshToken\ValueObject;
+
+use App\Module\Identity\Domain\RefreshToken\ValueObject\Token;
+use Tests\Shared\UnitTestCase;
+
+class TokenTest extends UnitTestCase
+{
+    /**
+     * @return void
+     */
+    public function testItShouldCreateAToken(): void
+    {
+        $token = $this->faker->uuid();
+        $tokenValueObject = Token::fromRaw($token);
+        $this->assertEquals($token, $tokenValueObject->toRaw());
+    }
+
+}
